5f9d264e913fe437c31f405c8e9bb7fc5b37bd8e
Colin Eberhardt <colin.eberhardt@gmail.com>
2014-07-06 18:07:53 +0100

    Initial Commit

---
+++ ReactiveSwiftFlickrSearch/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ReactiveSwiftFlickrSearch
+//
+//  Created by Colin Eberhardt on 06/07/2014.
+//  Copyright (c) 2014 Colin Eberhardt. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+                            
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
