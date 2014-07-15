d576e4408e002f9978cca2231682ecb5308fe5a2
Colin Eberhardt <colin.eberhardt@gmail.com>
2014-07-15 22:29:37 +0100

    Executes a search an initiates navigation

---
+++ ReactiveSwiftFlickrSearch/Util/RAC.swift
@@ -0,0 +1,9 @@
+//
+//  RAC.swift
+//  ReactiveSwiftFlickrSearch
+//
+//  Created by Colin Eberhardt on 15/07/2014.
+//  Copyright (c) 2014 Colin Eberhardt. All rights reserved.
+//
+
+import Foundation
